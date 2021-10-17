cd4521547011c2bd227447ad88412f26dd2ef0d1
rokosbasilisk <thecomplexitytheorist@gmail.com>
2021-10-18 00:52:08 +0530

    trying binary layer

--- mingpt/binary_model.py
+++ mingpt/binary_model.py
@@ -13,8 +13,61 @@ import logging
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from binary_modules import *
+#from binary_modules import *
 logger = logging.getLogger(__name__)
+'''
+binary-nn functions 
+'''
+from torch.autograd import Function
+
+
+class BinarizeF(Function):
+
+    @staticmethod
+    def forward(cxt, input):
+        output = input.new(input.size())
+        output[input >= 0] = 1
+        output[input < 0] = -1
+        return output
+
+    @staticmethod
+    def backward(cxt, grad_output):
+        grad_input = grad_output.clone()
+        return grad_input
+
+# aliases
+binarize = BinarizeF.apply
+
+
+class BinaryTanh(nn.Module):
+    def __init__(self):
+        super(BinaryTanh, self).__init__()
+        self.hardtanh = nn.Hardtanh()
+
+    def forward(self, input):
+        output = self.hardtanh(input)
+        output = binarize(output)
+        return output
+
+
+class BinaryLinear(nn.Linear):
+
+    def forward(self, input):
+        binary_weight = binarize(self.weight)
+        if self.bias is None:
+            return F.linear(input, binary_weight)
+        else:
+            return F.linear(input, binary_weight, self.bias)
+
+    def reset_parameters(self):
+        # Glorot initialization
+        in_features, out_features = self.weight.size()
+        stdv = math.sqrt(1.5 / (in_features + out_features))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
+        self.weight.lr_scale = 1. / stdv
 
 class GPTConfig:
     """ base GPT config, params common to all GPT versions """
