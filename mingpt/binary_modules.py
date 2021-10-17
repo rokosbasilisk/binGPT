c62f2c7cf596b4e3bcdb5585953a5e9d0ad43431
thor <aryasomayajula.b@mu-sigma.com>
2021-10-18 00:35:46 +0530

    add binary model

---
+++ mingpt/binary_modules.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 12:29:33 2018
+
+@author: akash
+"""
+
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from binary_functions import *
+
+
+class BinaryTanh(nn.Module):
+    def __init__(self):
+        super(BinaryTanh, self).__init__()
+        self.hardtanh = nn.Hardtanh()
+
+    def forward(self, input):
+        output = self.hardtanh(input)
+        output = binarize(output)
+        return output
+        
+
+class BinaryLinear(nn.Linear):
+
+    def forward(self, input):
+        binary_weight = binarize(self.weight)
+        if self.bias is None:
+            return F.linear(input, binary_weight)
+        else:
+            return F.linear(input, binary_weight, self.bias)
+
+    def reset_parameters(self):
+        # Glorot initialization
+        in_features, out_features = self.weight.size()
+        stdv = math.sqrt(1.5 / (in_features + out_features))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
+        self.weight.lr_scale = 1. / stdv
+
